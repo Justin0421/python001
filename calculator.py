1d14043139de60e20c420ead1facab9b414f0ae7
Justin0421 <zhigang.huang@hotmail.com>
2017-12-18 21:05:59 +0800

    file operation

--- calculator.py
+++ calculator.py
@@ -2,72 +2,50 @@
 
 import sys
 
-#constants
-i_tax = (0.03,0.1,0.2,0.25,0.3,0.35,0.45)
-ss_del = (0,105,555,1005,2755,5505,13505)
-s_insur = 0.08 + 0.02 + 0.005 + 0 + 0 +0.06 
-thred = 3500
-#get parameters
-w_nb = []
-w_income = []
-for arg in sys.argv[1:]:
-    w = arg.split(':')
-    #convert to int and test error
-    try:
-        int_wnb = int(w[0])
-        int_wincome = int(w[1])
-        if int_wnb <= 0 or int_wincome <= 0:
-            raise ParameterError
-    except:
-        print('ParameterError')
-    #get parameter lists
-    w_nb.append(w[0])
-    w_income.append(w[1])
+class Config(object):
+    def __init__(self, configfile):
+        with open(configfile) as file:
+            count = 0
+            self._config = {}
+            for line in file:
+                count += 1
+                L =  line.replace(" ","").split('=')
+                #print(line)
+                self._config[L[0]] = L[1]
+        #for k,v in self._config.items():
+            #print(k,v)
+    def get_config(self, value):
+        #for k,v in self._config.items():
+            #print(k, v)
+        #print(type(value))
+        #print(value)   
+        return self._config.get(value,'not exist key')
+    
+class UserData(object):
+    def __init__(self, userdatafile):
+        with open(userdatafile) as file:
+            count = 0
+            self._userdata = {}
+            for line in file:
+                count += 1
+                L =  line.replace(" ","").split(',')
+                self._userdata[L[0]] = L[1]
 
-#function check income class and return
-def check_case(payg):
-    if payg < 1500:
-        case = 0
-    elif payg < 4500:
-        case = 1
-    elif payg < 9000:
-        case = 2
-    elif payg < 35000:
-        case = 3
-    elif payg < 55000:
-        case = 4
-    elif payg < 80000:
-        case = 5
-    else:
-        case = 6
-    return case
+    def get_userdata(self, value):
+        return self._userdata.get(value,'not exist key')
+    def calculator(self)
+        pass
+    def dumptofile(self,outputfile)
+        pass
+#get all parameters
+args = sys.argv[1:]
+index = args.index('-c')
+configfile = args[index+1]
+index = args.index('-d')
+userdatafile = args[index+1]
 
-#calculate PAYG 
-def cal_payg(income):
-    payg = int(income)*(1 - s_insur) - thred
-    return payg
-
-#calculate tax
-def cal_tax(payg,cs):
-    tax = float(payg) * i_tax[cs] - ss_del[cs]
-    return tax
-
-if __name__ == '__main__':
-    payg = []
-    tax = []
-    i = 0
-    for income in w_income:
-        p = cal_payg(income)
-        if int(p) < 0:
-            t = 0
-        else:
-            payg.append(p)
-            cs = check_case(p)
-            t = cal_tax(p,cs)
-        tax.append(t)
-        #output result
-        final_income = int(income)*(1-s_insur) - t
-        print(w_nb[i],end ='')
-        print(':',end = '')
-        print(format(final_income, ".2f"))
-        i = i + 1
+#try class
+config = Config(configfile)
+print(config.get_config('JiShuH'))
+userdata = UserData(userdatafile)
+print(userdata.get_userdata('101'))
